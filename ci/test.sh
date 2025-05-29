f66f7b81ee6ef1cdbd91cc9d1156784b969d9adf
nachog00 <nachogeo@zingolabs.org>
2025-05-29 00:38:29 -0300

    Update Docker image in test script - Changed Docker image tag from `zaino-ci:local` to `zingodevops/zaino-ci:latest` in `ci/test.sh` - Ensures using the latest CI environment for testing

--- ci/test.sh
+++ ci/test.sh
@@ -3,5 +3,5 @@ docker run --rm \
   -v "$PWD/target":/home/zaino/target \
   -v "$PWD/docker_cargo/git":/home/zaino/.cargo/git \
   -v "$PWD/docker_cargo/registry":/home/zaino/.cargo/registry \
-  zaino-ci:local
+  zingodevops/zaino-ci:latest
 
