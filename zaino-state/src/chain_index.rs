79b41742a23995e578844b400b5fb80a36dbf42d
idky137 <idky137@gmail.com>
2025-06-02 15:13:14 +0100

    clippy fixes

--- zaino-state/src/chain_index.rs
+++ zaino-state/src/chain_index.rs
@@ -6,10 +6,10 @@
 //! - FinalisedState: Holds block data for the remainder of the best chain.
 //!
 //! - Chain: Holds chain / block structs used internally by the ChainIndex.
-//!        - Holds fields required to:
-//!          - a. Serve CompactBlock data dirctly.
-//!          - b. Build trasparent tx indexes efficiently
-//!        - NOTE: Full transaction and block data is served from the backend finalizer.
+//!   - Holds fields required to:
+//!     - a. Serve CompactBlock data dirctly.
+//!     - b. Build trasparent tx indexes efficiently
+//!   - NOTE: Full transaction and block data is served from the backend finalizer.
 
 pub mod finalised_state;
 pub mod mempool;
