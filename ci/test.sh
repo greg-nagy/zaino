cff4a204ed5d6b218b5720651f053f182251233e
nachog00 <nachogeo@zingolabs.org>
2025-06-02 14:51:04 -0300

    Revert "Merge branch 'ci-image-pipeline-root' into ci-image-pipeline-zcashd-registry"
    This reverts commit 170218d883f443610e7cb05928aaaa12fb66500a, reversing
    changes made to fffcfeb36b22a3cb9d61c5a02a9e34cc68f550c7.

--- ci/test.sh
+++ ci/test.sh
@@ -3,5 +3,5 @@ docker run --rm \
   -v "$PWD/target":/home/zaino/target \
   -v "$PWD/docker_cargo/git":/home/zaino/.cargo/git \
   -v "$PWD/docker_cargo/registry":/home/zaino/.cargo/registry \
-  zingodevops/zaino-ci:root
+  zingodevops/zaino-ci:latest
 
