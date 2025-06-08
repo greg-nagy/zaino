e911629ba4181e4998d1792fdcc918ea6a1a1373
al amoda <alamoda@tutamail.com>
2025-06-08 03:47:10 +0000

    adjust comments

--- zaino-fetch/src/lib.rs
+++ zaino-fetch/src/lib.rs
@@ -1,6 +1,6 @@
 //! A mempool-fetching, chain-fetching and transaction submission service that uses zcashd's JsonRPC interface.
 //!
-//! Used as a reference, legacy option, and for backwards compatibility.
+//! Usable as a backwards-compatible, legacy option.
 
 #![warn(missing_docs)]
 #![forbid(unsafe_code)]
