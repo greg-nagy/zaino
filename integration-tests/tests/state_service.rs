223e34b4b672bd4544245966e3b4e3a737ccb841
Hazel OHearn <gygaxis@zingolabs.org>
2025-06-11 18:39:59 -0300

    fix incorrect byte reversal in test

--- integration-tests/tests/state_service.rs
+++ integration-tests/tests/state_service.rs
@@ -1067,11 +1067,7 @@ mod zebrad {
             for _ in 0..5 {
                 test_manager.generate_blocks_with_delay(1).await;
                 assert_eq!(
-                    chaintip_subscriber
-                        .next_tip_hash()
-                        .await
-                        .unwrap()
-                        .bytes_in_display_order(),
+                    chaintip_subscriber.next_tip_hash().await.unwrap().0,
                     <[u8; 32]>::try_from(
                         state_service_subscriber
                             .get_latest_block()
