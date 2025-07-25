7cf35e078314d8a71a3be67d2bcdd27e393d56b7
al amoda <alamoda@tutamail.com>
2025-07-25 05:14:55 -0400

    Adjust comments

--- zaino-state/src/backends/fetch.rs
+++ zaino-state/src/backends/fetch.rs
@@ -226,11 +226,11 @@ impl ZcashIndexer for FetchServiceSubscriber {
 
     /// Returns details on the active state of the TX memory pool.
     ///
-    /// zcashd reference: [`getmempoolinfo`](https://zcash.github.io/rpc/getmempoolinfo.html)
+    /// online zcash rpc reference: [`getmempoolinfo`](https://zcash.github.io/rpc/getmempoolinfo.html)
     /// method: post
     /// tags: mempool
     ///
-    /// Original implementation: [`getmempoolinfo`](https://github.com/zcash/zcash/blob/18238d90cd0b810f5b07d5aaa1338126aa128c06/src/rpc/blockchain.cpp#L1555)
+    /// Canonical source code implementation: [`getmempoolinfo`](https://github.com/zcash/zcash/blob/18238d90cd0b810f5b07d5aaa1338126aa128c06/src/rpc/blockchain.cpp#L1555)
     ///
     /// Zebra does not support this RPC call directly.
     async fn get_mempool_info(&self) -> Result<GetMempoolInfoResponse, Self::Error> {
