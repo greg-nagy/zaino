8336b2639cab5346026ed7895e5fd6d6d13985c7
al amoda <alamoda@tutamail.com>
2025-09-18 20:57:49 -0400

    cleanup state chain-index

--- zaino-state/src/chain_index.rs
+++ zaino-state/src/chain_index.rs
@@ -433,7 +433,9 @@ impl<Source: BlockchainSource> NodeBackedChainIndex<Source> {
     pub fn status(&self) -> StatusType {
         let finalized_status = self.finalized_db.status();
         let mempool_status = self.mempool.status();
-        let combined_status = StatusType::from(self.status.load())
+        let combined_status = self
+            .status
+            .load()
             .combine(finalized_status)
             .combine(mempool_status);
         self.status.store(combined_status);
