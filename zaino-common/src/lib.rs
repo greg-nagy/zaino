67a5962b1f1b8afcfb0e5849c77ae834ece456df
al amoda <alamoda@tutamail.com>
2025-09-29 03:26:42 -0400

    rm common

--- zaino-common/src/lib.rs
+++ zaino-common/src/lib.rs
@@ -3,15 +3,12 @@
 //! This crate provides shared configuration types, network abstractions,
 //! and common utilities used across the Zaino blockchain indexer ecosystem.
 
-pub mod config;
 pub mod network;
 pub mod service;
 pub mod storage;
 
 // Re-export commonly used types for convenience
 
-// TODO is GrpcClientConfig a better name? Something else?
-pub use config::GrpcTlsConfig;
 pub use network::Network;
 pub use service::ServiceConfig;
 pub use storage::{CacheConfig, DatabaseConfig, DatabaseSize, StorageConfig};
