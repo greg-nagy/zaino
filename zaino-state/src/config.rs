6d3d94541b036da773cabf3329987954e915ddde
al amoda <alamoda@tutamail.com>
2025-10-05 18:00:50 -0400

    remove commented no_db code

--- zaino-state/src/config.rs
+++ zaino-state/src/config.rs
@@ -47,9 +47,6 @@ pub struct StateServiceConfig {
     /// Disables internal sync and stops zaino waiting on server sync.
     /// Used for testing.
     pub no_sync: bool,
-    // /// Disables FinalisedState.
-    // /// Used for testing.
-    // pub no_db: bool,
 }
 
 impl StateServiceConfig {
@@ -68,7 +65,6 @@ impl StateServiceConfig {
         storage: StorageConfig,
         network: Network,
         no_sync: bool,
-        // no_db: bool,
     ) -> Self {
         StateServiceConfig {
             validator_config,
@@ -82,7 +78,6 @@ impl StateServiceConfig {
             storage,
             network,
             no_sync,
-            // no_db,
         }
     }
 }
@@ -109,9 +104,6 @@ pub struct FetchServiceConfig {
     /// Disables internal sync and stops zaino waiting on server sync.
     /// Used for testing.
     pub no_sync: bool,
-    // /// Disables FinalisedState.
-    // /// Used for testing.
-    // pub no_db: bool,
 }
 
 impl FetchServiceConfig {
@@ -128,7 +120,6 @@ impl FetchServiceConfig {
         storage: StorageConfig,
         network: Network,
         no_sync: bool,
-        // no_db: bool,
     ) -> Self {
         FetchServiceConfig {
             validator_rpc_address,
@@ -140,7 +131,6 @@ impl FetchServiceConfig {
             storage,
             network,
             no_sync,
-            // no_db,
         }
     }
 }
