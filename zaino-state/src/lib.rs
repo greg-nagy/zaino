a44d76e09204a38b3ff2fc19c95da3583bb0fdc4
zancas <zancas@zingolabs.org>
2025-10-17 13:11:05 -0700

    fix typo

--- zaino-state/src/lib.rs
+++ zaino-state/src/lib.rs
@@ -56,7 +56,7 @@ pub use chain_index::mempool::{MempoolKey, MempoolValue};
 
 #[cfg(feature = "test_dependencies")]
 /// allow public access to additional APIs, for testing
-pub mod test_depenedencies {
+pub mod test_dependencies {
     /// Testing export of chain_index
     pub mod chain_index {
         pub use crate::chain_index::*;
