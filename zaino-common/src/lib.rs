1f36145cd4d1972a76e7140a88b9081b1ef9d109
nachog00 <nachogeo@zingolabs.org>
2025-11-08 21:15:38 -0300

    create zaino-common::config module and move stuff over there

--- zaino-common/src/lib.rs
+++ zaino-common/src/lib.rs
@@ -3,13 +3,4 @@
 //! This crate provides shared configuration types, network abstractions,
 //! and common utilities used across the Zaino blockchain indexer ecosystem.
 
-pub mod network;
-pub mod service;
-pub mod storage;
-pub mod validator;
-
-// Re-export commonly used types for convenience
-pub use network::Network;
-pub use service::ServiceConfig;
-pub use storage::{CacheConfig, DatabaseConfig, DatabaseSize, StorageConfig};
-pub use validator::ValidatorConfig;
+pub mod config;
